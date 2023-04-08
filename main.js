6f83fe301839eaf156353168322cce53ac8f3200
Mohammad Zafar <mzcoder7@gmail.com>
2023-04-08 14:59:06 +0100

    description of kata

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 /* 
-👉 Write your kata here!
+Can you find the encrypted message?
+You are given an array of numbers (from 0-25) and symbols
+Each number must be transformed to its equivalent letter in the alphabet
+E.g. if the number is 10, letter is 'k' (index 10)
+Replace each symbol with a space character. 
+Finally, join the transformed array to return a string with the hidden message
+
+EXAMPLE:
+findMessage([0, 11, 22, 0, 24, 18, '!', 1, 4, '?', 2, 14, 3, 8, 13, 6])
+should return 'always be coding'
+
+GOOD LUCK!
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
+export function findMessage(array) {}
